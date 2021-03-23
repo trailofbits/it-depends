de2c0f08ce1b24af95551f0583ccebbeac3cbe5d
Evan Sultanik <evan.sultanik@trailofbits.com>
2021-03-23 09:47:17 -0400

    Trying to get SQLalchemy not to segfault!

--- it_depends/db.py
+++ it_depends/db.py
@@ -1,5 +1,5 @@
 from pathlib import Path
-from typing import Dict, Union, Iterable, Iterator, Optional, FrozenSet
+from typing import Dict, FrozenSet, Iterable, Iterator, Optional, Tuple, Union
 
 from semantic_version import SimpleSpec, Version
 from sqlalchemy import Column, create_engine, distinct, ForeignKey, Integer, String, UniqueConstraint
@@ -7,49 +7,56 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.ext.hybrid import hybrid_property
 from sqlalchemy.orm import relationship, sessionmaker
 
-from .dependencies import (
-    Dependency as DepDependency, DependencyClassifier, Package as DepPackage, SemanticVersion, PackageCache
-)
+from .dependencies import Dependency, DependencyClassifier, Package, SemanticVersion, PackageCache
 
 DEFAULT_DB_PATH = Path.home() / ".config" / "it-depends" / "dependencies.sqlite"
 
 Base = declarative_base()
 
 
-class Dependency(Base, DepDependency):
+class DBDependency(Base, Dependency):
     __tablename__ = "dependencies"
 
     id = Column(Integer, primary_key=True)
     from_package_id = Column(Integer, ForeignKey("packages.id"))
-    from_package = relationship("Package", back_populates="raw_dependencies")
+    from_package = relationship("DBPackage", back_populates="raw_dependencies")
     package = Column(String, nullable=False)
-    _version = Column("version", String, nullable=True)
+    _semantic_version = Column("semantic_version", String, nullable=True)
 
     __table_args__ = (
-        UniqueConstraint("from_package_id", "package", "version", name="dependency_unique_constraint"),
+        UniqueConstraint("from_package_id", "package", "semantic_version", name="dependency_unique_constraint"),
     )
 
-    def __init__(self, package: "Package", dep: DepDependency):
+    def __init__(self, package: "DBPackage", dep: Dependency):
+        # We intentionally skip calling super().__init__()
         self.from_package_id = package.id
         self.package = dep.package
-        self.version = dep.semantic_version
+        self.semantic_version = dep.semantic_version
 
     @hybrid_property
-    def version(self) -> SemanticVersion:
-        return SimpleSpec.parse(self._version)
+    def semantic_version(self) -> SemanticVersion:
+        return SimpleSpec.parse(self._semantic_version)
 
-    @version.setter
-    def version(self, new_version: Union[SemanticVersion, str]):
-        self._version = str(new_version)
+    @semantic_version.setter
+    def semantic_version(self, new_version: Union[SemanticVersion, str]):
+        self._semantic_version = str(new_version)
 
 
-class DependencyMapping(Dict[str, Dependency]):
-    def __init__(self, package: "Package"):
+class DependencyMapping:
+    def __init__(self, package: "DBPackage"):
         super().__init__()
-        self._package: Package = package
         self._deps: Dict[str, Dependency] = {
-            dep.package: dep for dep in self._package.raw_dependencies
+            dep.package: Dependency(dep.package, dep.semantic_version) for dep in package.raw_dependencies
         }
+
+    def items(self) -> Iterator[Tuple[str, Dependency]]:
+        yield from self._deps.items()
+
+    def keys(self) -> Iterable[str]:
+        return self._deps.keys()
+
+    def values(self) -> Iterable[Dependency]:
+        return self._deps.values()
 
     def __setitem__(self, dep_name: str, dep: Dependency):
         self._deps[dep_name] = dep
@@ -67,7 +74,7 @@ class DependencyMapping(Dict[str, Dependency]):
         return iter(self._deps)
 
 
-class Package(Base, DepPackage):
+class DBPackage(Base, Package):
     __tablename__ = "packages"
 
     id = Column(Integer, primary_key=True)
@@ -79,24 +86,30 @@ class Package(Base, DepPackage):
         UniqueConstraint("name", "version", "source", name="package_unique_constraint"),
     )
 
-    raw_dependencies = relationship("Dependency", back_populates="from_package", cascade="all, delete, delete-orphan")
+    raw_dependencies = relationship("DBDependency", back_populates="from_package", cascade="all, delete, delete-orphan")
 
-    def __init__(self, package: DepPackage):
+    def __init__(self, package: Package):
+        # We intentionally skip calling super().__init__()
         self.name = package.name
         self.version = package.version
         self.source = package.source
 
     @staticmethod
-    def from_package(package: DepPackage, session) -> "Package":
-        if not isinstance(package, Package):
+    def from_package(package: Package, session) -> "DBPackage":
+        if not isinstance(package, DBPackage):
             dep_pkg = package
-            package = Package(package)
+            package = DBPackage(package)
             session.add(package)
             session.flush()
-            session.add_all([Dependency(package, dep) for dep in dep_pkg.dependencies.values()])
+            session.add_all([DBDependency(package, dep) for dep in dep_pkg.dependencies.values()])
         else:
             session.add(package)
         return package
+
+    def to_package(self) -> Package:
+        return Package(name=self.name, version=self.version, dependencies=(
+            Dependency(package=dep.package, semantic_version=dep.semantic_version) for dep in self.raw_dependencies
+        ), source=self.source)
 
     @property
     def version(self) -> Version:
@@ -118,24 +131,25 @@ class SourceFilteredPackageCache(PackageCache):
         self.parent: DBPackageCache = parent
 
     def __len__(self):
-        return self.parent.session.query(Package).filter(Package.source.like(self.source)).count()
+        return self.parent.session.query(DBPackage).filter(DBPackage.source.like(self.source)).count()
 
     def __iter__(self) -> Iterator[Package]:
-        yield from self.parent.session.query(Package).filter(Package.source.like(self.source)).all()
+        yield from [p.to_package()
+                    for p in self.parent.session.query(DBPackage).filter(DBPackage.source.like(self.source)).all()]
 
     def from_source(self, source: Optional[str]) -> "PackageCache":
         return SourceFilteredPackageCache(source, self.parent)
 
     def package_versions(self, package_name: str) -> Iterator[Package]:
-        return self.parent.session.query(Package).filter(
-            Package.name.like(package_name), Package.source.like(self.source)
-        ).all()
+        yield from [p.to_package() for p in self.parent.session.query(DBPackage).filter(
+            DBPackage.name.like(package_name), DBPackage.source.like(self.source)
+        ).all()]
 
     def package_names(self) -> FrozenSet[str]:
-        return frozenset(self.parent.session.query(distinct(Package.name))
-                         .filter(Package.source.like(self.source)).all())
+        return frozenset(self.parent.session.query(distinct(DBPackage.name))
+                         .filter(DBPackage.source.like(self.source)).all())
 
-    def match(self, to_match: Union[str, Package, Dependency]) -> Iterator[Package]:
+    def match(self, to_match: Union[str, Package, DBDependency]) -> Iterator[Package]:
         return self.parent.match(to_match, source=self.source)
 
     def add(self, package: Package, source: Optional[DependencyClassifier] = None):
@@ -174,10 +188,10 @@ class DBPackageCache(PackageCache):
     def session(self):
         return self._session
 
-    def add(self, package: DepPackage, source: Optional[DependencyClassifier] = None):
+    def add(self, package: Package, source: Optional[DependencyClassifier] = None):
         self.extend((package,), source=source)
 
-    def extend(self, packages: Iterable[DepPackage], source: Optional[DependencyClassifier] = None):
+    def extend(self, packages: Iterable[Package], source: Optional[DependencyClassifier] = None):
         for package in packages:
             for existing in self.match(package):
                 if len(existing.dependencies) > len(package.dependencies):
@@ -190,39 +204,46 @@ class DBPackageCache(PackageCache):
                 continue
             if package.source is None and source is not None:
                 package.source = source.name
-            if isinstance(package, Package):
+            if isinstance(package, DBPackage):
                 self.session.add(package)
             else:
-                _ = Package.from_package(package, self.session)
+                _ = DBPackage.from_package(package, self.session)
         self.session.commit()
 
     def __len__(self):
-        return self.session.query(Package).count()
+        return self.session.query(DBPackage).count()
 
     def __iter__(self) -> Iterator[Package]:
-        return self.session.query(Package).all()
+        yield from [p.to_package() for p in self.session.query(DBPackage).all()]
 
     def from_source(self, source: Optional[str]) -> SourceFilteredPackageCache:
         return SourceFilteredPackageCache(source, self)
 
     def package_versions(self, package_name: str) -> Iterator[Package]:
-        return self.session.query(Package).filter(Package.name.like(package_name)).all()
+        yield from [p.to_package()
+                    for p in self.session.query(DBPackage).filter(DBPackage.name.like(package_name)).all()]
 
     def package_names(self) -> FrozenSet[str]:
-        return frozenset(result[0] for result in self.session.query(distinct(Package.name)).all())
+        return frozenset(result[0] for result in self.session.query(distinct(DBPackage.name)).all())
 
-    def match(self, to_match: Union[str, DepPackage, DepDependency], source: Optional[str] = None) -> Iterator[Package]:
+    def match(
+            self, to_match: Union[str, Package, Dependency], source: Optional[str] = None
+    ) -> Iterator[Package]:
         if source is not None:
-            filters = (Package.source.like(source),)
+            filters = (DBPackage.source.like(source),)
         else:
             filters = ()
-        if isinstance(to_match, DepPackage):
-            yield from self.session.query(Package).filter(
-                Package.name.like(to_match.name), Package.version_str.like(str(to_match.version)), *filters
-            ).all()
-        elif isinstance(to_match, DepDependency):
+        if isinstance(to_match, Package):
+            # we intentionally build a list before yielding so that we don't keep the session query lingering
+            yield from [p.to_package() for p in self.session.query(DBPackage).filter(
+                DBPackage.name.like(to_match.name), DBPackage.version_str.like(str(to_match.version)), *filters
+            ).all()]
+        elif isinstance(to_match, Dependency):
             for package in self.match(to_match.package, source=source):
                 if package.version in to_match.semantic_version:
                     yield package
         else:
-            yield from self.session.query(Package).filter(Package.name.like(to_match), *filters).all()
+            # we intentionally build a list before yielding so that we don't keep the session query lingering
+            yield from [p.to_package() for p in self.session.query(DBPackage).filter(
+                DBPackage.name.like(to_match), *filters
+            ).all()]
