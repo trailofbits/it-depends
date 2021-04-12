05db6660d59f778bb38e0cca5d474e3e590d822f
Evan Sultanik <evan.sultanik@trailofbits.com>
2021-04-12 10:48:23 -0400

    Git hooks to lint and test on commit/push

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setup(
         "tqdm>=4.48.0"
     ],
     extras_require={
-        "dev": ["flake8", "pytest", "twine"]
+        "dev": ["flake8", "pytest", "twine", "mypy"]
     },
     entry_points={
         "console_scripts": [
