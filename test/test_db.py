ee04889b19b537e1892f410a016e18cdf8c6e1b1
feliam <felipe.andres.manzano@gmail.com>
2021-04-29 14:20:33 -0300

    Fix tests

--- test/test_db.py
+++ test/test_db.py
@@ -1,14 +1,22 @@
 from unittest import TestCase
 
 from it_depends.db import DBPackageCache
-from it_depends.dependencies import Dependency, Package, SimpleSpec, Version
+from it_depends.dependencies import Dependency, Package, SimpleSpec, Version, DependencyClassifier
 
+class UnknownClassifier(DependencyClassifier):
+    name = "unknown"
+    description = "unknown classifier"
+    def can_classify(self, repo) -> bool:
+        return False
 
+    def classify(self, repo, cache):
+        pass
+        
 class TestDB(TestCase):
     def test_db(self):
         with DBPackageCache() as cache:
-            pkg = Package(name="package", version=Version.coerce("1.0.0"),
-                          dependencies=(Dependency("dep", SimpleSpec(">3.0")),))
+            pkg = Package(name="package", version=Version.coerce("1.0.0"), source=UnknownClassifier().default_instance(),
+                          dependencies=(Dependency(package="dep", semantic_version=SimpleSpec(">3.0"), source=UnknownClassifier().default_instance()),))
             cache.add(pkg)
             self.assertIn(pkg, cache)
             self.assertEqual(len(cache), 1)
@@ -16,5 +24,5 @@ class TestDB(TestCase):
             cache.add(pkg)
             self.assertEqual(len(cache), 1)
             # try adding the package again, but with fewer dependencies:
-            smaller_pkg = Package(name="package", version=Version.coerce("1.0.0"))
+            smaller_pkg = Package(name="package", version=Version.coerce("1.0.0"), source=UnknownClassifier().default_instance())
             self.assertRaises(ValueError, cache.add, smaller_pkg)
