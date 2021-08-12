1d7f3915c97b7f70d70b84d4d0cb93de73465c69
Evan Sultanik <evan.sultanik@trailofbits.com>
2021-08-12 13:27:14 -0400

    Adds expected output for the Bitcoin test

--- test/test_smoke.py
+++ test/test_smoke.py
@@ -121,6 +121,7 @@ class TestSmoke(TestCase):
     def test_cargo(self, package_list):
         pass
 
+    # @gh_smoke_test("brix", "crypto-js", "971c31f0c931f913d22a76ed488d9216ac04e306")
     @gh_smoke_test("bitcoin", "bitcoin", "4a267057617a8aa6dc9793c4d711725df5338025")
     def test_npm(self, package_list):
         pass
